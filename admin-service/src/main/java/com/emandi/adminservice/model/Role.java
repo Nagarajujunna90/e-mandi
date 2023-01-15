801932b6a6a45b8bdc5a8f5524f02a6cb34592aa
njunna <nagarajujunna90@gmail.com>
2023-01-15 18:39:07 +0530

    updated pom.xml

--- admin-service/src/main/java/com/emandi/adminservice/model/Role.java
+++ admin-service/src/main/java/com/emandi/adminservice/model/Role.java
@@ -1,9 +1,9 @@
 package com.emandi.adminservice.model;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Set;
 @Data
 @NoArgsConstructor
